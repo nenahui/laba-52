2e665ee1db7e0834c2905e14c22ce3d40de073fc
nenahui <kanatraccoon@gmail.com>
2024-05-20 20:10:47 +0600

    Finished fitst level

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Card from './components/Card/Card.tsx';
 
 function App() {
   return (
     <>
+      <Card rank={'j'} suit={'spades'}/>
     </>
   );
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,20 @@
-const Card = () => {
+import React from 'react';
+
+interface ICardProps {
+  rank: string;
+  suit: string;
+}
+
+const Card: React.FC<ICardProps> = ({rank, suit}) => {
   return (
-    <>
-    </>
+    <div className="playingCards faceImages">
+      <span className={`card rank-${rank} ${suit}`}>
+        <span className="rank">{rank.toUpperCase()}</span>
+        <span className="suit">{
+          suit === 'diams' ? '♦' : suit === 'hearts' ? '♥' : suit === 'spades' ? '♠' : suit === 'clubs' ? '♣' : 'Invalid suit'
+        }</span>
+      </span>
+    </div>
   );
 };
 
